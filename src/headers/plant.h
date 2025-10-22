270d3fc2b0febbdb7ef738b31ea7fba9cc32e8e2
Sashumi-6 <danielcohen448@gmail.com>
2025-10-22 12:05:03 +0200

    skeleton for plant done - state

---
+++ src/headers/plant.h
@@ -0,0 +1,56 @@
+#ifndef PLANT_H
+#define PLANT_H
+
+#include <string>
+#include "garden.h"
+
+class Plant : public GardenComponent {
+    public:
+        void waterPlant();
+        void exposeToSunlight();
+        void loseWater();
+        bool canSell();
+        void grow();
+        void setState(PlantState* state);
+
+    private:
+        int price;
+        std::string name;
+};
+
+class PlantState {
+    public:
+        virtual void handleWaterPlant() = 0;
+        virtual void handleExposeToSunlight() = 0;
+        virtual bool canSell() = 0;
+        virtual void handleGrow() = 0;
+
+    protected:
+        GardenComponent* plant;
+};
+
+class SeedlingState : public PlantState {
+    public:
+        void handleWaterPlant();
+        void handleExposeToSunlight();
+        bool canSell();
+        void handleGrow();
+};
+
+class MatureState : public PlantState {
+    public:
+        void handleWaterPlant();
+        void handleExposeToSunlight();
+        bool canSell();
+        void handleGrow();
+};
+
+class DeadState : PlantState {
+    public:
+        void handleWaterPlant();
+        void handleExposeToSunlight();
+        bool canSell();
+        void handleGrow();
+};
+
+#endif
