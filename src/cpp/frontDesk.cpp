aa4c5415737f3108982fb5dfc82cd2a4d7443f7b
Ntuuu <mbatha05n@gmail.com>
2025-11-03 19:36:01 +0200

    added implementation to executeAllCommands function

--- src/cpp/frontDesk.cpp
+++ src/cpp/frontDesk.cpp
@@ -1,5 +1,36 @@
 #include "frontDesk.h"
 
+void FrontDesk::addCommand(Command* cmd) {
+    commands.push_back(cmd);
+}
+
+void FrontDesk::addEmployee(Employee* emp) {
+    employees.push_back(emp);
+}
+
+void FrontDesk::executeAllCommands() {
+    for (Command* cmd : commands) {
+        // Employee* emp = getAvailableEmployee<Employee>();
+        if (cmd->getType() == MAINTENANCE_COMMAND || cmd->getType() == PLANT_COMMAND) {
+            Cashier* cashier = getAvailableEmployee<Cashier>();
+            if (cashier) {
+                cmd->execute(cashier);
+            } else {
+                Caretaker* caretaker = getAvailableEmployee<Caretaker>();
+                if (caretaker) {
+                    cmd->execute(caretaker);
+                }
+            }
+        } else if (cmd->getType() == REQUEST_COMMAND) {
+            Employee* emp = getAvailableEmployee<Employee>();
+            if (emp) {
+                cmd->execute(emp);
+            }
+        }
+    }
+    commands.clear();
+}
+
 bool FrontDesk::placeOrder(std::vector<ProductRequest>& requests, std::string customerName){
     if(requests.empty()) return false;
 
@@ -24,3 +55,5 @@ T* FrontDesk::getAvailableEmployee() {
     }
     return nullptr;
 }
+
+
