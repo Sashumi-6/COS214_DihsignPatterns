e2262c948e86803a0515fb9d33b4306185f99f21
morganthegirlboss <u24910882@tuks.co.za>
2025-11-03 21:49:51 +0200

    added customer files with request code done

---
+++ src/headers/Customer.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+//we call a customer constructor in simulation, we destroy customer at end too
+
+#include <string>
+// #include "command.h"
+// #include "frontDesk.h"
+
+class Customer{
+
+    private:
+    bool order; // if true then customer wants order, if false, they want to ask a question
+    std::string name;
+
+    public:
+    Customer();
+    Customer* createCustomerOrder();
+    void createCustomerQuery();
+    
+};
+
+#endif
