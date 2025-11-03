0d55932e2508ddfa8eed95d22051079eba12cb67
Do-nny <DonovanNelson05@gmail.com>
2025-11-03 23:28:21 +0200

    Implemented  utility iterators as well as a rename for the iterator that handles the plants specifically

--- src/cpp/iterator.cpp
+++ src/cpp/iterator.cpp
@@ -1,8 +1,8 @@
 #include "../headers/iterator.h"
 
-GardenIterator::GardenIterator(GardenComponent* root) : root(root) { first(); }
+PlantOnlyIterator::PlantOnlyIterator(GardenComponent* root) : root(root) { first(); }
 
-GardenComponent* GardenIterator::first() {
+GardenComponent* PlantOnlyIterator::first() {
     while (!stack.empty()) {
         stack.pop();
     }
@@ -12,7 +12,7 @@ GardenComponent* GardenIterator::first() {
     return next();
 }
 
-GardenComponent* GardenIterator::next() {
+GardenComponent* PlantOnlyIterator::next() {
     while (!stack.empty()) {
         GardenComponent* curr = stack.top();
         stack.pop();
@@ -28,4 +28,66 @@ GardenComponent* GardenIterator::next() {
     return nullptr;
 }
 
-bool GardenIterator::isDone() const { return stack.empty(); }
+bool PlantOnlyIterator::isDone() const { return stack.empty(); }
+
+SectionOnlyIterator::SectionOnlyIterator(GardenComponent* root) : root(root) {
+    first();
+}
+
+GardenComponent* SectionOnlyIterator::first() {
+    while (!stack.empty()) {
+        stack.pop();
+    }
+    if (root != nullptr) {
+        stack.push(root);
+    }
+    return next();
+}
+
+GardenComponent* SectionOnlyIterator::next() {
+    while (!stack.empty()) {
+        GardenComponent* curr = stack.top();
+        stack.pop();
+
+        std::vector<GardenComponent*> children = curr->getChildren();
+        for (auto it = children.rbegin(); it != children.rend(); ++it) {
+            stack.push(*it);
+        }
+
+        if (!curr->isLeaf()) {
+            return curr;
+        }
+    }
+    return nullptr;
+}
+
+bool SectionOnlyIterator::isDone() const { return stack.empty(); }
+
+FullIterator::FullIterator(GardenComponent* root) : root(root) { first(); }
+
+GardenComponent* FullIterator::first() {
+    while (!queue.empty()) {
+        queue.pop();
+    }
+    if (root != nullptr) {
+        queue.push(root);
+    }
+    return next();
+}
+
+GardenComponent* FullIterator::next() {
+    while (!queue.empty()) {
+        GardenComponent* curr = queue.front();
+        queue.pop();
+        std::vector<GardenComponent*> children = curr->getChildren();
+        for (auto* child : children) {
+            if (child != nullptr) {
+                queue.push(child);
+            }
+        }
+        return curr;
+    }
+    return nullptr;
+}
+
+bool FullIterator::isDone() const { return queue.empty(); }
